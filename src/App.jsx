db75df0516a406c0ae6c74a3c20a5bafdeedb5fd
Christina Vekri <christina.vek@gmail.com>
2023-12-14 16:18:56 +0100

    updated the html and Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([])
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="container">
+
+        <h1>Get your Stuff together List 📝</h1>
+
+        <form>
+          <div id="formDiv">
+          <input type="text" placeholder="What do you need to get done?" id="item"/>
+          <button id='addBtn' type="submit"><span className="material-symbols-outlined">
+            +
+            </span></button>
+
+          </div>
+
+          <div id="clearBtnDiv">
+            <button id="clearBtn">Clear All Tasks</button>
+            <button id="hideBtn" type="button" onclick="hideCompleted()">Hide Completed</button>
+
+          </div>
+        </form>
+
+
+        <div id="taskSections">
+
+          <div id="toDoItemsDiv">
+            <h2>TO DO:</h2>
+            <div className="myListItems">
+            </div>
+          </div>
+
+          <div id="completedItemsDiv">
+            <h2>COMPLETED:</h2>
+            <div className="myCompletedItems">
+            </div>
+          </div>
+
+        </div>
+
+
+        </div>
+
     </>
   )
 }
